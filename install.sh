54a8b3a449cf60dab23405bfd1b2e734660be019
bkc39 <bkc39@cornell.edu>
2014-05-30 16:46:52 -0400

    Fix the install script.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 ## Installs the .sty and .cls files on the local machine
-## Version 1
-## Feb 2, 2014
+## Version 2
+## May 30, 2014
 ## bkc
 
-export TEXDIR=`kpsewhich -var-value=TEXMFHOME`
-cp *.sty *.cls $(TEXDIR)
+TEXDIR=`kpsewhich -var-value=TEXMFHOME`
+cp *.sty *.cls $TEXDIR
 texhash
